aaa62770dd59237f0e0ecfaeec2dca6eb3649485
qoo35790 <jocelyn821121@gmail.com>
2020-09-23 01:35:48 +0800

    20200923 論壇留言MVC

--- Car98-mvc/src/main/java/com/web/car98/forum/controller/CommentController.java
+++ Car98-mvc/src/main/java/com/web/car98/forum/controller/CommentController.java
@@ -11,64 +11,96 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.web.car98.forum.model.CommentBean;
 import com.web.car98.forum.service.CommentService;
 import com.web.car98.forum.service.impl.CommentServiceImpl;
+import com.web.car98.member.model.LoginBean;
 
 @Controller
 public class CommentController {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@Autowired
 	CommentService commentservice;
 
-	@RequestMapping(value = "/comment", method = RequestMethod.POST)
-	public String insertCom(Model model ,HttpServletRequest request, HttpServletResponse response)
+	@RequestMapping(value = "/forum/comment.do", method = RequestMethod.POST)
+	public String insertCom(Model model, HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		int floor = 2;
+
 		Map<String, String> errorMessage = new HashMap<>();
-		request.setAttribute("ErrorMsgKey", errorMessage);
+		model.addAttribute("ErrorMsgKey", errorMessage);
 		request.setCharacterEncoding("UTF-8");
+		LoginBean loginBean = new LoginBean();
+		loginBean = (LoginBean) model.getAttribute("loginBean");
 		Integer comId = null;
 		String comText = request.getParameter("comments");
-		Integer postId = 1;
-		Integer memId = 36;
-		Integer comLike = 6;
+		Integer postId = Integer.parseInt(request.getParameter("postId"));
+		Integer memId = Integer.parseInt(request.getParameter("memId"));
+		Integer comLike = 0;
 		if (comText == null || comText.trim().length() == 0) {
 			errorMessage.put("TextEmptyError", "請輸入內文");
 		}
 		if (errorMessage.isEmpty()) {
-//			CommentBean cb = new CommentBean(postId, comText, new Date(), comLike);
-			try {
-				CommentServiceImpl service = new CommentServiceImpl();
-//				service.insertCom(cb);
-				List<CommentBean> resultList = new ArrayList<>();
-				resultList = service.selectCom(postId);
-				request.setAttribute("CommentBean", resultList);
-				request.setAttribute("floor", floor);
-				RequestDispatcher rd = request.getRequestDispatcher("/forum/talktalk.jsp");
-				rd.forward(request, response);
-//				return;
-			} catch (ServletException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			CommentBean cb = new CommentBean(postId, memId, comText, new Date(), comLike);
+
+			CommentServiceImpl service = new CommentServiceImpl();
+			service.insertCom(cb);
+			List<CommentBean> resultList = new ArrayList<>();
+			resultList = service.selectCom(postId);
+			model.addAttribute("CommentBean", resultList);
+			return "/forum/talktalk";
+
 		} else {
-			RequestDispatcher rd = request.getRequestDispatcher("/forum/talktalk.jsp");
-			rd.forward(request, response);
-//			return;
+
+			return "/forum/talktalk";
 		}
-		return "/comment";
 	}
 
+	@RequestMapping(value = "/forum/updateCom.do", method = RequestMethod.POST)
+	public String updateCom(Model model, HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Map<String, String> errorMessage = new HashMap<>();
+		model.addAttribute("ErrorMsgKey", errorMessage);
+		request.setCharacterEncoding("UTF-8");
+		String comIdStr = request.getParameter("comId");
+		System.out.println(comIdStr);
+		Integer comId = Integer.valueOf(comIdStr);
+		CommentBean commentBean = null;
+		CommentServiceImpl service = new CommentServiceImpl();
+		
+		int n = 0;
+		n = service.updateComByPk(commentBean);
+		if(n != 0) {
+			return "/forum/talktalk";
+		}else {
+			
+		}
+		return "/forum/talktalk";
+
+	}
+
+	@RequestMapping(value = "/forum/deleteCom.do", method = RequestMethod.POST)
+	public String deleteCom(Model model, HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Map<String, String> errorMessage = new HashMap<>();
+		model.addAttribute("ErrorMsgKey", errorMessage);
+		request.setCharacterEncoding("UTF-8");
+		String comIdStr = request.getParameter("comId");
+		Integer comId = Integer.valueOf(comIdStr);
+
+		CommentServiceImpl service = new CommentServiceImpl();
+		service.deleteComByPk(comId);
+		return "/forum/talktalk.jsp";
+	}
 }
