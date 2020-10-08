72a9b3d4dd50c0c7c996e0bb7c3d7b3ef15112e8
asd7920 <asd7920@gmail.com>
2020-10-08 15:07:26 +0800

    租車contrlloer更新

--- Car98-mvc/src/main/java/com/web/car98/rentcar/controller/RentCarController.java
+++ Car98-mvc/src/main/java/com/web/car98/rentcar/controller/RentCarController.java
@@ -1,52 +1,85 @@
 package com.web.car98.rentcar.controller;
 
-import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.web.car98.rentcar.model.RentCarBean;
 import com.web.car98.rentcar.service.RentCarService;
 
 @Controller
+//@RequestMapping(value = "/rentcar", method = RequestMethod.GET)
 public class RentCarController {
-
+	
 	@Autowired 
-	RentCarService service;
-	
-//	第一個下拉式選單
-	@GetMapping("/area")
-	@ResponseBody 
-	protected List<RentCarBean> doGet(){
-		List<RentCarBean> list = service.showCityDistMenu();
-		return list;
-	}
-	
-//	取得符合地區的資料(經過使用者篩選)
-//	@ResponseBody
-//	public List<RentCarBean> getRentCarsByDist(String city , String district) {
-//		List<RentCarBean> rentcarbean = this.service.getRentCarsByDist(city, district);
-//		return rentcarbean;
-//	}
-	@PostMapping("/dist")
-	protected @ResponseBody List<RentCarBean> doPost(HttpServletRequest request,
-			@RequestBody RentCarBean rb,
-			Model model) throws Exception {
-		List<RentCarBean> rentcarbean = service.getRentCarsByDist(rb.getCity() ,rb.getDistrict());
-		model.addAttribute("rcbean", rentcarbean);
+	RentCarService rentCarService;
 		
-		return rentcarbean;
+	@GetMapping("/area")
+	@ResponseBody
+	protected List<RentCarBean> doGet(){
+		List<RentCarBean> areaData = rentCarService.showArea();
+			return areaData;
+		}
+
+//		CarTypeServiceImpl cts = new CarTypeServiceImpl();
+//		ServletContext sc = getServletContext();
+//		WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(sc);
+//		CarTypeService cts = ctx.getBean(CarTypeService.class);
+//		Collection<CarTypeBean> ctb = cts.getCarTypeData();
+//
+//		Gson gson = new Gson();
+//		String carJsonString = gson.toJson(ctb);
+//		PrintWriter out = response.getWriter();
+//		response.setContentType("application/json");
+//		response.setCharacterEncoding("UTF-8");
+//		out.print(carJsonString);
+//		System.out.println(carJsonString);
+//		out.flush();
 	}
-}
+
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//			throws ServletException, IOException {
+//		request.setCharacterEncoding("UTF-8");
+//		//先取出session物件
+//		HttpSession session = request.getSession(false); 
+//		
+//		// 如果舊的session物件不存在， 不要建立新的Session物件，直接傳回 null，請使用者登入
+//		if (session == null) {
+//			response.sendRedirect(response.encodeRedirectURL(
+//					request.getContextPath() + "/login/BSlogin.jsp"));
+//			return;
+//		}
+//		
+//		// 取出存放在session物件內的RentCarBean物件
+//		RentCarBean rcb = (RentCarBean)session.getAttribute("RentCarBean");
+//		
+//		// 如果找不到RentCarBean物件，就新建一個
+//		if (rcb == null) {
+//			rcb = new RentCarBean();
+//			// 並將此新建RentCarBean的物件放到session物件內，成為它的屬性物件
+//			session.setAttribute("queryRent", rcb);
+//			}
+//		
+//		// 1. 讀取使用者輸入查詢的資料
+//		String city = request.getParameter("city");
+//		String district = request.getParameter("district") + "區";
+//		
+//		// 2. 進行必要的資料轉換
+//		
+//		// 3. 檢查資料
+//		
+//		// 4. 進行商業邏輯運算
+////		RentCarService rentCarservice = new RentCarServiceImpl();
+////		Spring的寫法		
+////		RentCarService rentCarservice = ctx.getBean(RentCarService.class);
+//		
+//		// 5.依照商業邏輯運算的結果，來挑選適當的
+//		RequestDispatcher rd = request.getRequestDispatcher("carRent.jsp");
+//		rd.forward(request, response);
+//		return;
+//	}
+//	
+
